3445921173e27fd7a1e4c09f4ea4e5872cf61aff
Houssem Dellai <houssem.dellai@live.com>
2019-08-19 18:53:59 +0200

    Add project files.

---
+++ AppTemplate/AppTemplate/MainTemplate.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppTemplate
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    [ContentProperty("Child")]
+    public partial class MainTemplate : ContentView
+    {
+        public MainTemplate()
+        {
+            InitializeComponent();
+        }
+
+        public string PageTitle
+        {
+            get => HeaderLabel.Text;
+            set => HeaderLabel.Text = value;
+        }
+
+        public View Body
+        {
+            get => BodyContent.Content;
+            set => BodyContent.Content = value;
+        }
+
+        public View Footer
+        {
+            get => FooterContent.Content;
+            set => FooterContent.Content = value;
+        }
+    }
+}
